42976863114ffc1c415074b4f89d0d91928b5326
defaultuser0 <defaultuser0@DESKTOP-7QRNG3C>
2025-10-06 13:24:26 +0300

    Task2

---
+++ Tyuiu.Ahmadi1.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.Ahmadi1.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.Ahmadi1.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "ahmadi mohamad hussain";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, ahmadi mohamad hussain", res);
+        }
+    }
+}
---
+++ Tyuiu.Ahmadi1.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.Ahmadi1.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.Ahmadi1.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed38fc1e-71aa-4316-8c9c-eddd558ca86c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
