ca83c3f14761de98f7e6e5befce9a57017785a5b
defaultuser0 <defaultuser0@DESKTOP-7QRNG3C>
2025-10-09 13:07:19 +0300

    Task3

---
+++ Tyuiu.Ahmadi1.Sprint0.Task3.V0.Tast/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.Ahmadi1.Sprint0.Task3.V0.Tast")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.Ahmadi1.Sprint0.Task3.V0.Tast")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6bf0571-02a6-4205-a120-b15e7fafd10c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
